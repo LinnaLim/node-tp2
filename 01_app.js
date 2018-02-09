9bc2e6ba3922e52ef912e91a5f1edfebfdb94495
Linna Lim <e1479687@cmaisonneuve.qc.ca>
2018-02-09 15:26:51 -0500

    Ajout de la connexion avec Mongodb

--- 01_app.js
+++ 01_app.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 app.use(express.static('public'));
-
+const bodyParser= require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+const MongoClient = require('mongodb').MongoClient;
 /* on associe le moteur de vue au module «ejs» */
 app.set('view engine', 'ejs'); // générateur de template
+
+
+
+var db // variable qui contiendra le lien sur la BD
+
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+	// lancement du serveur Express sur le port 8081
+	app.listen(8081, () => {
+		console.log('connexion à la BD et on écoute sur le port 8081')
+	});
+});
+
 
 
 app.get('/', function (req, res) {
@@ -17,11 +33,4 @@ app.get('/', function (req, res) {
         let resultat = JSON.parse(data);           
   		res.render('gabarit.ejs', {adresses: resultat});
   	});
-});
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
 });
