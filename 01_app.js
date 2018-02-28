54b3688470e5b8ce0ccb5df8977dbb2e0bd97ba3
Linna Lim <e1479687@cmaisonneuve.qc.ca>
2018-02-28 09:11:55 -0500

    Ajout de i18n et cookie-parser

--- 01_app.js
+++ 01_app.js
@@ -2,18 +2,23 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-
+// fs
 const fs = require('fs');
-
 // Body-parser
 const bodyParser= require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
-
 //MongoDb
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 /* on associe le moteur de vue au module «ejs» */
 app.set('view engine', 'ejs'); // générateur de template
+// i18n
+const i18n = require("i18n")
+/* Ajouter l'objet i18n à l'objet global «res» */
+app.use(i18n.init);
+// Cookie-parser
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 const peupler = require("./modules/peupler");
 let util =  require("util");
@@ -33,9 +38,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
 app.get('/', function (req, res) {
 	let cursor = db.collection('adresse').find().toArray((err, resultat) =>{
 		if (err) return console.log(err);
-		//console.log(JSON.stringfy(resultat));
-		// transfert du contenu vers la vue index.ejs (renders)
-		// affiche le contenu de la BD 
+		// affiche l'accueil'
 	  	res.render('accueil.ejs', {adresses: resultat});
   	});
 });
@@ -44,9 +47,7 @@ app.get('/', function (req, res) {
 app.get('/adresses', function (req, res) {
 	let cursor = db.collection('adresse').find().toArray((err, resultat) =>{
 		if (err) return console.log(err);
-		//console.log(JSON.stringfy(resultat));
-		// transfert du contenu vers la vue index.ejs (renders)
-		// affiche le contenu de la BD 
+		// affiche la page adresses
 		let cle = "";
 		let ordre = "";
 	  	res.render('adresses.ejs', {adresses: resultat, cle, ordre });
@@ -69,6 +70,7 @@ app.get('/ajouter', (req, res) => {
 	let infoListe = {"prenom":"","nom":"","telephone":"","courriel":""};
 	db.collection('adresse').save( infoListe, (err, result) => {
 		if (err) return console.log(err);
+		//ramene à la page Membres
 		res.redirect('/adresses');
 	});
 });
@@ -77,7 +79,7 @@ app.get('/ajouter', (req, res) => {
 app.post('/modifier', (req, res) => {
     //console.log('req.body' + req.body);
     //let util = require("util");
-
+    // Objet avec les champs modifiés
     let oModif = {
         "_id": ObjectID(req.body['_id']),
         prenom: req.body.prenom,
@@ -85,10 +87,11 @@ app.post('/modifier', (req, res) => {
         telephone: req.body.telephone,
         courriel: req.body.courriel
     }    
-
+    // Sauver l'objet dans la BDD
     db.collection('adresse').save(oModif, (err, result) => {
         if (err) return console.log(err);
-        console.log('sauvegarder dans la BD');
+        //console.log('sauvegarder dans la BD');
+        //ramene à la page Membres
         res.redirect('/adresses');
     });
 });
@@ -109,6 +112,7 @@ app.get('/peupler', (req, res) => {
 	let infoPeupler = peupler();
 	db.collection('adresse').insert(infoPeupler, (err, result) => {
 		if (err) return console.log(err);
+		//ramene à la page Membres
 		res.redirect('/adresses');
 	});
 });
@@ -117,6 +121,7 @@ app.get('/peupler', (req, res) => {
 app.get('/vider', (req, res) => {
 	db.collection('adresse').remove(req.body, (err, result) => {
 		if (err) return console.log(err);
+		//ramene à la page Membres
 		res.redirect('/adresses');
 	});
 });
@@ -124,7 +129,9 @@ app.get('/vider', (req, res) => {
 
 //////////////////////////////////////////////////// Route /rechercher
 app.post('/rechercher', (req, res) => {
+	// Trouver le mot de recherche
 	let inputRecherche = req.body.requete;
+	// Verifier le texte mit dans le input dans toute les categorie de la BDD
     let requete = { 
     				$or: [ 
     						{ 'prenom': {'$regex': '^' + inputRecherche,  '$options' : 'i' }},
@@ -134,12 +141,14 @@ app.post('/rechercher', (req, res) => {
     				]
     			};
 
+    // Trouver l'élément chercher dans la BDD
     db.collection('adresse').find(requete).toArray((err, resultat) => {
-    	console.log(resultat);
+    	//console.log(resultat);
         if (err) return console.log(err);
-        console.log('sauvegarder dans la BD');
+        //console.log('sauvegarder dans la BD');
         let cle = "";
 		let ordre = "";
+		// Affiche la page d'adresses
         res.render('adresses.ejs', {adresses: resultat, cle, ordre });
     });
 });
@@ -150,9 +159,32 @@ app.get('/profil/:id', (req, res) =>{
 	let id  = ObjectID(req.params.id);
 	let cursor = db.collection('adresse').find({'_id': id}).toArray((err, resultat) =>{
 		if (err) return console.log(err);
-		//console.log(JSON.stringfy(resultat));
-		// transfert du contenu vers la vue index.ejs (renders)
-		// affiche le contenu de la BD 
+		// affiche de la page du profil du membre
 	  	res.render('profil.ejs', {adresses: resultat});
   	});
+});
+
+
+///////////////////////////////////////////////////// Route /en
+app.get('/en', (req, res) => {
+	// 'en' est enregistré comme langue
+	res.setLocale('en');
+	// on en profite pour sauver la langue dans un cookie
+	res.cookie('moncookie', 'en');
+	// retourne le catalogue
+	console.log('res.getCatalog() = ' + res.getCatalog());
+	// retourne la langue qui a été choisie
+	console.log('res.getLocale() = ' + res.getLocale());
+	var bienvenue = 'hello';
+	console.log('en= ' + res.__('bienvenue');
+
+});
+
+///////////////////////////////////////////////////// Route /en ou fr
+// Traduire MENU et TH de Tableau!!!!!!!!!
+app.get('/:locale(en|fr',  (req, res) => {
+	// on récupère le paramètre de l'url pour enregistrer la langue
+	res.setLocale(req.params.locale);
+	// on peut maintenant traduire
+	console.log('res.__(leMotAtraduire) = ' + res.__(leMotAtraduire));
 });
